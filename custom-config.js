c9e1f4326f384d6b1946c0730febeb9d0695e05a
AlexanderWiechert <alexander.wiechert@bundesliga.de>
2019-10-07 15:24:27 +0200

    tt

--- custom-config.js
+++ custom-config.js
@@ -1,10 +1,15 @@
 module.exports = {
   extends: 'lighthouse:default',
   settings: {
-    audits: [
-      'first-meaningful-paint',
-      'first-cpu-idle',
-      'byte-efficiency/uses-optimized-images',
-    ],
-  },
+  categories: {
+    performance: {
+      title: 'Performance',
+      description: 'This category judges your performance',
+      auditRefs: [
+        {id: 'first-meaningful-paint', weight: 2, group: 'metrics'},
+        {id: 'first-cpu-idle', weight: 3, group: 'metrics'},
+        {id: 'interactive', weight: 5, group: 'metrics'},
+      ],
+    }
+  }
 };
